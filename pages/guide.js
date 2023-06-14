4482545e08aff79d1e880d8bff82e474b9175c66
Faizan Asad <p176056@nu.edu.pk>
2023-06-15 00:01:32 +0500

    hjg

--- pages/guide.js
+++ pages/guide.js
@@ -36,7 +36,7 @@ function guide() {
 
         <li className='pt-1'>Go to USDC contract <a href='https://etherscan.io/token/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48#writeProxyContract' target={'_blank'}>USDC Contract Address</a></li>
 
-        <li  className='pt-1'>On Approve Function in <span className='bg-blue-500 px-1 rounded'>spender</span> field enter Hitmonbox's Contract address </li> <span className=' bg-red-500 rounded-full px-2'>0x407d2a779F0281DB7AE363970D4dd27916090987</span>
+        <li  className='pt-1'>On Approve Function in <span className='bg-blue-500 px-1 rounded'>spender</span> field enter Hitmonbox Contract address </li> <span className=' bg-red-500 rounded-full px-2'>0x407d2a779F0281DB7AE363970D4dd27916090987</span>
 
         <li  className='pt-1'>On Approve Function in <span className='bg-blue-500 px-1 rounded'>value</span> field enter total USDC for number of nfts you want to mint. i.e </li><span className=' bg-red-500 rounded-full px-2'>600 x (number of NFTS) x 1000000</span>
 
@@ -47,7 +47,7 @@ function guide() {
         <li>On Mint Function in <span className='bg-blue-500 px-1 rounded'>_mintAmount</span> field enter number of Hitmonbox NFTs you want to mint </li> <span className=' bg-red-500 rounded-full px-2 '>(number of NFTs) i.e 2</span>
 
 
-        <div className='pt-10 text-sm'><span className="bg-blue-700 rounded-l px-2 ">NOTE:</span> Make sure you approve right amount of USDC for the no. of NFTs you're minting</div>
+        <div className='pt-10 text-sm'><span className="bg-blue-700 rounded-l px-2 ">NOTE:</span> Make sure you approve right amount of USDC for the no. of NFTs youre minting</div>
 
 <div className="flex justify-center gap-2 text-black font-bold">
   <div className="bg-red-500 rounded-3xl px-3 py-2 my-3">  <Link className="text-black" href="https://hitmonbox.com/investors" target="_blank">Investors</Link></div>
